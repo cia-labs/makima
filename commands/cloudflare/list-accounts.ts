4fb3cf144e075d1ae82be72305810c646441f832
root <root@makima001.cialabs.tech>
2023-12-09 13:07:31 +0000

    chore: misc changes

--- commands/cloudflare/list-accounts.ts
+++ commands/cloudflare/list-accounts.ts
@@ -1,26 +1,46 @@
-import { CacheType, ChatInputCommandInteraction, MessageContextMenuCommandInteraction, SlashCommandBuilder, UserContextMenuCommandInteraction } from "discord.js";
+import {
+  CacheType,
+  ChatInputCommandInteraction,
+  MessageContextMenuCommandInteraction,
+  SlashCommandBuilder,
+  UserContextMenuCommandInteraction,
+} from "discord.js";
 import { getEmailRoutingAddresses } from "@/lib/cf";
 
 const command = new SlashCommandBuilder()
-    .setName('users')
-    .setDescription('Lists all cialabs accounts');
+  .setName("users")
+  .setDescription("Lists all cialabs accounts");
 
 export const GetAccountsCommand = {
-    command,
-    async execute(interaction: ChatInputCommandInteraction<CacheType> | MessageContextMenuCommandInteraction<CacheType> | UserContextMenuCommandInteraction<CacheType>) {
-        try {
-            const response = await getEmailRoutingAddresses();
-            const accounts = response.data.result;
-            await interaction.reply(`fetching list of accounts`);
-            if (accounts.length === 0) {
-                await interaction.editReply('No accounts found.');
-            } else {
-                const accountList = accounts.map(account => `- ${account.email} ${account.verified ? '✅' : '❌'}`).join('\n');
-                await interaction.editReply(`List of accounts:\n${accountList}`);
-            }
-        } catch (error) {
-            console.error('Error fetching accounts:', error);
-            await interaction.editReply('An error occurred while fetching accounts.');
-        }
+  command,
+  async execute(
+    interaction:
+      | ChatInputCommandInteraction<CacheType>
+      | MessageContextMenuCommandInteraction<CacheType>
+      | UserContextMenuCommandInteraction<CacheType>
+  ) {
+    try {
+      const response = await getEmailRoutingAddresses();
+      const accounts = response.data.result;
+      await interaction.reply({
+        content: `Fetching user list`,
+        ephemeral: true,
+      });
+      if (accounts.length === 0) {
+        await interaction.editReply("No accounts found.");
+      } else {
+        const accountList = accounts
+          .map(
+            (account) => `- ${account.email} ${account.verified ? "✅" : "❌"}`
+          )
+          .join("\n");
+        await interaction.channel?.send({
+          content: `Users:\n${accountList}`,
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching accounts:", error);
+      await interaction.editReply("An error occurred while fetching accounts.");
     }
-}
+  },
+};
