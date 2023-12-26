2d37ac87ded0c2f359a6d5cebdc0ec0c0fcc53be
Raj Sharma <raj.fps2000@gmail.com>
2023-12-26 19:44:31 +0530

    fix: listing flow fixed

--- commands/cloudflare/list-accounts.ts
+++ commands/cloudflare/list-accounts.ts
@@ -23,25 +23,21 @@ export const GetAccountsCommand = {
       const response = await getEmailRoutingAddresses();
       const accounts = response.data.result;
 
-      const status_message = await interaction.reply({
-        content: `Fetching user list`,
-        ephemeral: true,
-      });
       if (accounts.length === 0) {
-        await interaction.editReply("No accounts found.");
+        await interaction.reply("No accounts found.");
       } else {
         const accountList = accounts
           .map((e) => e.matchers.find((m) => m.field === "to")?.value)
           .filter((v) => v)
           .join("\n");
-        await status_message.delete();
-        await interaction.channel?.send({
+
+        await interaction.reply({
           content: `## Users\n${accountList}`,
         });
       }
     } catch (error) {
       console.error("Error fetching accounts:", error);
-      await interaction.editReply("An error occurred while fetching accounts.");
+      await interaction.reply("An error occurred while fetching accounts.");
     }
   },
 };
